3744d6314234626bd9fe2a49de9d3db615df57e6
niwa-niwa <niwayama1101@gmail.com>
2021-01-21 20:15:11 +0900

    use setState and constructor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SeasonDisplay from './SeasonDisplay'
+// import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = { lat: null }
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called setState
+                this.setState({ lat: position.coords.latitude})
+                // DO NOT right this
+                // this.state.lat = lat: position.coords.latitude 
+            } ,
+            (err) => console.log(err)
+        )
+    }
+
     // Classed Component have to extend React.Component
     // and override render methods
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        )
-    
-        return <div>Latitude: </div>    
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
