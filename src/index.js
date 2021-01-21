c60a553cb5d6c7e5acfa33d558d74c1ef16c6896
niwa-niwa <niwayama1101@gmail.com>
2021-01-22 00:42:49 +0900

    refactoring to each component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component{
 
@@ -39,10 +40,10 @@ class App extends React.Component{
         }
 
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
-        return <div>Loading!</div>
+        return <Spinner />
 
     }
 
