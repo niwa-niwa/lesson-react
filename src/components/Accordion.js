f71f81dc412b6952fd5a01c89c89e310ca9219fe
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-24 19:01:53 +0900

    refactoried

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Accordion = ({ items }) => {
 
-    const renderedItems = items.map( item => {
+    const [activeIndex, setActiveIndex] = useState(null)
+
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+    }
+
+
+    const renderedItems = items.map( (item, index) => {
+
+        const active = index === activeIndex ? 'active' : ''
+
         return (
             <React.Fragment key={ item.title }>
-                <div className="title active">
+                <div
+                    className={ `title ${active}` }
+                    onClick={ () => onTitleClick(index) } // This case clicking triggered function
+                    // onClick={ onTitleClick(index) } //This case rendering triggered function
+                >
                     <i className="dropdown icon"></i>
                     { item.title }
                 </div>
-                <div className="content active">
+                <div className={ `content ${active}` }>
                     <p>{ item.content }</p>
                 </div>
             </React.Fragment>
@@ -21,6 +36,7 @@ const Accordion = ({ items }) => {
     return(
         <div className="ui styled accordion">
             { renderedItems }
+            <h1>{ activeIndex }</h1>
         </div>
 
     )
