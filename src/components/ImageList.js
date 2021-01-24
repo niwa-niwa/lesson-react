2c457fbcb242544590fdd62bc126f442b17808f7
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-24 12:26:16 +0900

    made grid photos with React.createRef

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,4 +1,7 @@
+import './ImageList.css'
 import React from 'react'
+import ImageCard from './ImageCard'
+
 
 const ImageList = props => {
 
@@ -6,12 +9,16 @@ const ImageList = props => {
     //     return <img key={image.id} src={image.urls.regular} />
     // })
     
-    const images = props.images.map(({ description, id, urls }) => {
-        return <img alt={description} key={id} src={urls.regular} />
-    })
+    // const images = props.images.map(({ description, id, urls }) => {
+    //     return <img alt={description} key={id} src={urls.regular} />
+    // })
 
+    const images = props.images.map(image => {
+        return <ImageCard key={image.id} image={image} />
+    })
+    
     return(
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     )
 }
 export default ImageList
