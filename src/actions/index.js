d8f30346ee9ef9eb2e2acf13c6eb1fca98a808e0
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-29 23:34:29 +0900

    refactoried fetchPostAndUsers with chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,18 @@ import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+
+    await dispatch(fetchPosts()) // get posts
+
+    _.chain(getState().posts) // set posts that they were got  the above
+        .map('userId') // mapping userIds that each post
+        .uniq() // filtered unique userIds 
+        .forEach(id => dispatch(fetchUser(id))) //get userIds
+        .value()
+}
+
+
 export const fetchPosts = () => async dispatch => {
 
     const response = await jsonPlaceholder.get('/posts')
@@ -9,27 +21,6 @@ export const fetchPosts = () => async dispatch => {
     dispatch( {type: 'FETCH_POSTS', payload: response.data} )
 
 }
-
-
-// How to request with cache pattern 1
-// export const fetchUser = (id) => async dispatch => {
-
-//     const  response = await jsonPlaceholder.get(`/users/${id}`)
-
-//     dispatch({ type: 'FETCH_USER', payload: response.data})
-// }
-
-
-// How to request with cache pattern 2
-// export const fetchUser = _.memoize(function(id){
-
-//     return async function(dispatch){
-        
-//         const  response = await jsonPlaceholder.get(`/users/${id}`)
-        
-//         dispatch({ type: 'FETCH_USER', payload: response.data})
-//     }
-// })
 
 
 // How to request with cache pattern 3
